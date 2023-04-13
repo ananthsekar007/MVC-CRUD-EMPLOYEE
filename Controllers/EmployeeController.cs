a8798df557d22c58d47f9f465d30b49f86c94556
ananthsekar007 <ananthsekar007@gmail.com>
2023-04-13 11:24:26 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using MVCProj.Models;
+using NuGet.Protocol.Core.Types;
+using System.Data;
+
+namespace MVCProj.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IConfiguration _configuration;
+        SqlConnection _Connection;
+        public EmployeeController(IConfiguration configuration) {
+            _configuration = configuration;
+            _Connection = new SqlConnection(_configuration.GetConnectionString("Employee"));
+        }
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> Employees = new();
+            _Connection.Open();
+            SqlCommand cmd = new SqlCommand("FETCH_EMPLOYEES", _Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                EmployeeModel employee = new();
+                employee.Id = (int)reader["id"];
+                employee.Name = (string)reader["name"];
+                employee.Department = (string)reader["department"];
+                employee.Salary = (decimal)reader["salary"];
+                employee.Dob = (DateTime)reader["dob"];
+                employee.BaseLocation = (string)reader["base_location"];
+                Employees.Add(employee);
+            }
+
+            reader.Close();
+            _Connection.Close();
+
+            return Employees;
+        }
+
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View(GetEmployees());
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCProj.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Dob { get; set; }
+        public string BaseLocation { get; set; }
+    }
+}
